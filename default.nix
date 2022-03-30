c09add7fde5a23c2b2eea33ef6eb0c63e42508e4
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2022-03-30 22:53:04 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -12,6 +12,10 @@ rustPlatform.buildRustPackage rec {
   pname = "entman";
   version = "unstable";
 
+  meta = with pkgs.lib; {
+    platforms = platforms.linux;
+  };
+
   src = ./.;
 
   cargoSha256 = "0affpkd4aqyymgzazka697fyhmwscdb0i6vxnbm1f4pfb0136dkc";
